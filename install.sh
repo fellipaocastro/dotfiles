a419a6b061940f7bd6ada7d0f113b24b93523dd1
Fellipe Castro <contact@fellipecastro.com>
2016-01-02 11:23:46 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ rm -rf ~/dotfiles
 git clone https://github.com/fellipecastro/dotfiles.git ~/dotfiles
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-source  ~/.gitconfig
 
 ln -s ~/dotfiles/.zsh_aliases ~/.zsh_aliases
 source  ~/.zsh_aliases
