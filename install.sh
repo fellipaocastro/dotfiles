437e4a64b416bf81c01056cf934c2095fc376274
Fellipe Castro <contact@fellipecastro.com>
2016-01-02 10:48:33 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-git clone https://github.com/fellipecastro/gitconfig.git ~/gitconfig
-ln -s ~/gitconfig/.gitconfig ~/.gitconfig
+git clone https://github.com/fellipecastro/dotfiles.git ~/dotfiles
+
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+source  ~/.gitconfig
+
+ln -s ~/dotfiles/.zsh_aliases ~/.zsh_aliases
+source  ~/.zsh_aliases
