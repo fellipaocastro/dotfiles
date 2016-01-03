c15e72f5459fc7661839c4e2dfa9b9ce897a6a44
Fellipe Castro <contact@fellipecastro.com>
2016-01-03 10:58:46 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ rm -f ~/.gitconfig
 rm -f ~/.config
 rm -f ~/.zsh_aliases
 rm -f ~/.zsh_functions
-rm -f ~/.tmux.conf
+rm -f ~/.tmux-conf
 
 rm -rf ~/dotfiles
 
@@ -13,8 +13,8 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.config ~/.config
 ln -s ~/dotfiles/.zsh_aliases ~/.zsh_aliases
 ln -s ~/dotfiles/.zsh_functions ~/.zsh_functions
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.tmux-conf ~/.tmux-conf
 
 source  ~/.zsh_aliases
 source  ~/.zsh_functions
-tmux source-file ~/.tmux.conf
+tmux source-file ~/.tmux-conf
