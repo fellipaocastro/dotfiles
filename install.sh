81ac301fb31716fb28938f1aa341dd2bcb3575ba
Fellipe Castro <contact@fellipecastro.com>
2016-01-08 16:44:12 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 rm -f ~/.gitconfig
-rm -f ~/.config/flake8
 rm -f ~/.zsh_aliases
 rm -f ~/.zsh_functions
 rm -f ~/.tmux.conf
+rm -f ~/.rubocop.yml
+rm -f ~/.config/flake8
 
 rm -rf ~/dotfiles
 
@@ -17,6 +18,7 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.zsh_aliases ~/.zsh_aliases
 ln -s ~/dotfiles/.zsh_functions ~/.zsh_functions
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
 ln -s ~/dotfiles/flake8 ~/.config/flake8
 
 source  ~/.zsh_aliases
